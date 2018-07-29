9914d9b2b10de0555b506ee9e07ee444b5f5c7af
舟木正憲 <funakimfunaki@gmail.com>
2018-07-29 22:42:05 +0900

    heroku

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
+  console.log(req.body.events);
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
@@ -32,30 +33,24 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  let replyText = '';
-  if (event.message.text === '天気教えて') {
-    console.log(event.source.userId);
-    replyText = 'ちょっとまってね';
-    getNodeVer(event.source.userId);
+  let mes = ''
+  if (event.message.text === '天気教えて！') {
+    mes = 'ちょっとまってね'; //待ってねってメッセージだけ先に処理
+    getNodeVer(event.source.userId); //スクレイピング処理が終わったらプッシュメッセージ
   } else {
-    replyText = event.message.text;
+    mes = event.message.text;
   }
 
-  // create a echoing text message
-  let echo = {
+  return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: replyText
-  };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+    text: mes
+  });
 }
 
-let getNodeVer = (userId) => {
+const getNodeVer = async (userId) => {
   const res = await axios.get('http://weather.livedoor.com/forecast/webservice/json/v1?city=400040');
   const item = res.data;
 
