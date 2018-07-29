973f8f9061d2fe638eb5de28e1c31c85ac95f6bc
舟木正憲 <funakimfunaki@gmail.com>
2018-07-29 22:09:00 +0900

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const line = require('@line/bot-sdk');
-const async = require('async');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
