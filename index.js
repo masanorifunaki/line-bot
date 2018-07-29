f3e17d9f63ee0b3d8deee4c38bd312133a60438d
舟木正憲 <funakimfunaki@gmail.com>
2018-07-29 21:13:57 +0900

    heroku

--- index.js
+++ index.js
@@ -9,24 +9,29 @@ const config = {
   channelSecret: process.env.channelSecret
 };
 
+// create LINE SDK client
 const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
-  console.log(req.body.events);
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
 });
 
-
-
+// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
@@ -40,6 +45,7 @@ function handleEvent(event) {
   return client.replyMessage(event.replyToken, echo);
 }
 
+// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
